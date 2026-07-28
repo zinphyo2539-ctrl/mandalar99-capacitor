57bcce93bdd81fd0d26dbadc82e1d757e6416708
Zin Phyo <zinphyo2539@gmail.com>
2026-07-28 16:48:58 +0630

    Initial Mandalar99 Capacitor Android wrapper

---
+++ android/app/src/main/java/com/mandalar99/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mandalar99.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.js
@@ -0,0 +1,11 @@
+const config = {
+  appId: "com.mandalar99.app",
+  appName: "Mandalar99",
+  webDir: "dist",
+  server: {
+    url: "https://mdr99.onrender.com",
+    cleartext: false
+  }
+};
+
+module.exports = config;
